ed60ace5ea816f900125554b6d2ec77f5fde6ecb
totoqqwe <anton.stepanchenko14@gmail.com>
2023-11-23 20:23:29 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp14/FunctionCache.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    internal class FunctionCache<TKey, TResult>
+    {
+        private readonly Dictionary<TKey, CacheItem> cache = new Dictionary<TKey, CacheItem>();
+
+        public delegate TResult FuncDelegate(TKey key);
+
+        private class CacheItem
+        {
+            public TResult Result { get; set; }
+            public DateTime ExpirationTime { get; set; }
+        }
+
+        public TResult Execute(FuncDelegate func, TKey key, TimeSpan cacheDuration)
+        {
+            if (cache.TryGetValue(key, out var cacheItem) && DateTime.Now < cacheItem.ExpirationTime)
+            {
+                return cacheItem.Result;
+            }
+
+            TResult result = func(key);
+
+            cache[key] = new CacheItem
+            {
+                Result = result,
+                ExpirationTime = DateTime.Now.Add(cacheDuration)
+            };
+
+            return result;
+        }
+    }
+}
---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FunctionCache<string, int> cache = new FunctionCache<string, int>();
+            string inputKey = "example";
+
+            int result1 = cache.Execute(GetValue, inputKey, TimeSpan.FromSeconds(5));
+            Console.WriteLine($"Result 1: {result1}");
+
+            int result2 = cache.Execute(GetValue, inputKey, TimeSpan.FromSeconds(5));
+            Console.WriteLine($"Result 2: {result2}");
+
+            System.Threading.Thread.Sleep(6000);
+
+            int result3 = cache.Execute(GetValue, inputKey, TimeSpan.FromSeconds(5));
+            Console.WriteLine($"Result 3: {result3}");
+        }
+
+        static int GetValue(string key)
+        {
+            Console.WriteLine("Executing function to get value...");
+            return key.GetHashCode();
+        }
+    }
+    }
+}
